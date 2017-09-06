91b09d0700e5cd67ea13110f6d21b703f478cc50
puneet bindal <puneetbindal8989@gmail.com>
2017-09-06 12:33:41 +0530

    Delete pwd.c

--- pwd.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main() {
-    char cwd[1024];
-    chdir("/path/to/change/directory/to");
-    getcwd(cwd, sizeof(cwd));
-    printf("Current working dir: %s\n", cwd);
-}
