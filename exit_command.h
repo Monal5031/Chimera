e076e9befe54c8a7598f35fbf28913c6be248da2
kumarhimanshu5128409 <kumarhimanshu5128409@gmail.com>
2017-09-10 18:56:11 +0530

    updated exit command

--- exit_command.h
+++ exit_command.h
@@ -1,27 +1,7 @@
-void exit_command() {
-  /* code */
-  while (1) {
-		/* code */
-		std::cout << "Type exit to quit the program" << '\n';
-	  std::string command;
-	  std::cin >> command;
-	  if (command == "exit" || command == "Exit" || command == "quit") {
-	    /* code */
-			std::cout << "type Y to continue" << '\n';
-			std::string confirm;
-			std::cin >> confirm;
-			if (confirm == "Y" || confirm == "y") {
-				/* code */
-				
-				return;
-
-			}
-			else{
-				break;
-			}
-	    }
-	  else{
-	  std::cout << "Please enter valid commad" << '\n';;
-	}
-	}
+//#include <unistd.h>
+//#include <signal.h>
+int exit_command()
+{
+    kill(getppid(), SIGKILL);
+    return 0;
 }
