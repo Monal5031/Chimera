bd2e58fc9bc925c6d220715c24e8036aa18f79ad
Ajay Guru <201651005@iiitvadodara.ac.in>
2017-09-11 21:29:35 +0530

    commented

--- pwd.h
+++ pwd.h
@@ -4,4 +4,23 @@ int pwd() {
     getcwd(cwd, sizeof(cwd));
     cout << cwd << endl;
     return 0;
+    /*
+
+    #define _POSIX_SOURCE
+    #include <unistd.h>
+
+    char *getcwd(char *buffer, size_t size);
+
+
+
+    Determines the path name of the working directory and stores it in buffer.
+    size
+    The number of characters in the buffer area.
+    buffer
+    The name of the buffer that will be used to hold the
+    path name of the working directory.
+    buffer must be big enough to hold the working directory name,
+    plus a terminating NULL to mark the end of the name.
+    */
+
 }
