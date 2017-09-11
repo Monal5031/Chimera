34493671135bfe2c7cdc1aa798107aeda362ead4
Ajay Guru <201651005@iiitvadodara.ac.in>
2017-09-11 21:27:28 +0530

    commented

--- exit_command.h
+++ exit_command.h
@@ -1,4 +1,7 @@
 int exit_command() {
     kill(getppid(), SIGKILL);
+    //kills the process with certain ppid
+    //getppid() gets the pid of the certain process
+    //SIGKILL sends the process signal to kill that process
     return 0;
 }
