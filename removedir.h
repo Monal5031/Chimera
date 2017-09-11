ea0a9bc6914272adb40e79fc8f6211270045e119
Ajay Guru <201651005@iiitvadodara.ac.in>
2017-09-11 21:30:11 +0530

    commented

--- removedir.h
+++ removedir.h
@@ -6,10 +6,19 @@ int remdir(string dirname) {
 	}
 	char c[1000];
 	strcpy(c,dirname.c_str());
+	/*Copies the C string pointed by source into the array pointed by destination,
+	including the terminating null character (and stopping at that point).
+	To avoid overflows, the size of the array pointed by destination shall be
+	long enough to contain the same C string as source (including the terminating null character),
+	 and should not overlap in memory with source.
+	*/
+	// rmdir removes the given directory and returns 0 for successful removal
+	// and -1 for Error
+	
 	if(rmdir(c) == 0) {
 		cout << "The directory has been deleted." << endl;
 	} else {
-		cout << "No such directory exists, or if it does, it is non-empty." << endl;
+		cout << q"No such directory exists, or if it does, it is non-empty." << endl;
 	}
     return 0;
 }
